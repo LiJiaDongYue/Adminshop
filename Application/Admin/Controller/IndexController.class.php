6ef39c733dd8ad37139b940391bee5cdec1a4184
LiJiaDongYue <lijiadongyue@gmail.com>
2016-01-07 14:58:57 +0800

    修改页面
    主页修改正常

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -8,6 +8,27 @@ class IndexController extends Controller {
         $this->display("index");
     }
 
+    public function right()
+    {
+    	//获取系统开发信息
+    	$system_info=array(
+
+    		//框架版本
+    		"thinkphp"=>THINK_VERSION,
+    		//php版本
+    		"php"=>PHP_VERSION,
+    		//真实ip
+    		"ip"=>GetIp(),
+    		//操作系统
+    		"os"=>PHP_OS,
+    		//Apache
+    		"apache"=>apache_get_version(),
+    		);
+
+    	$this->assign('system_info',$system_info);
+    	$this->display("right");
+    }
+
     
 }
 
---
+++ Application/Admin/Controller/MemberController.class.php
@@ -0,0 +1,17 @@
+<?php
+namespace Admin\Controller;
+use Think\Controller;
+/**
+* 后台用户管理
+*/
+class MemberController extends Controller
+{
+
+	public function right()
+	{
+		
+		$this->display("right");
+	}
+	
+	
+}
